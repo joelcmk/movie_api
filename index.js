7b1edbcbf9482d6e5c7ea2d3b9e2ac87234826f1
joelcmk <57118300+joelcmk@users.noreply.github.com>
2020-02-29 19:30:00 -0600

    removed authentication logic to see the list of movies

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(cors({
 
 
 // Gets the list of data about all the movies
-app.get("/movies", passport.authenticate('jwt', { session: false }), function(req, res) {
+app.get("/movies", function(req, res) {
   Movies.find()
    .then(function(movies) {
      res.status(201).json(movies);
