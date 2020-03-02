696c0f48a9206f989f42a998a9b5d13a280a8b25
joelcmk <57118300+joelcmk@users.noreply.github.com>
2020-03-02 14:41:05 -0600

    added main-view.jsx

---
+++ client/src/components/main-view/main-view.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import axios from 'axios';
+
+export class MainView extends React.Component {
+  constructor() {
+    // Call the superclass constructor
+    // so React can initialize it
+    super();
+
+    // Initialize the state to an empty object so we can destructure it later
+    this.state = {};
+  }
+
+  // One of the "hooks" available in a React Component
+  componentDidMount() {
+    axios.get('<my-flix-2020.herokuapp.com/movies>')
+      .then(response => {
+        // Assign the result to the state
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  render() {
+    // If the state isn't initialized, this will throw on runtime
+    // before the data is initially loaded
+    const { movies } = this.state;
+
+    // Before the movies have been loaded
+    if (!movies) return <div className="main-view" />;
+
+    return (
+      <div className="main-view">
+        {movies.map(movie => (
+          <div className="movie-card" key={movie._id}>{movie.Title}</div>
+        ))}
+      </div>
+    );
+  }
+}
