5e36383206f9cf1fe0f93fd2e12f726c5a148d2b
joelcmk <57118300+joelcmk@users.noreply.github.com>
2020-03-03 13:36:57 -0600

    Added main-view, movie-card and movie view

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import axios from 'axios';
 
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
+
 export class MainView extends React.Component {
   constructor() {
     // Call the superclass constructor
@@ -8,7 +11,10 @@ export class MainView extends React.Component {
     super();
 
     // Initialize the state to an empty object so we can destructure it later
-    this.state = {};
+    this.state = {
+      movies: null,
+      selectedMovie: null
+    };
   }
 
   // One of the "hooks" available in a React Component
@@ -25,19 +31,28 @@ export class MainView extends React.Component {
       });
   }
 
+  onMovieClick(movie) {
+    this.setState({
+      selectedMovie: movie
+    });
+  }
+
   render() {
     // If the state isn't initialized, this will throw on runtime
     // before the data is initially loaded
-    const { movies } = this.state;
+    const { movies, selectedMovie } = this.state;
 
     // Before the movies have been loaded
     if (!movies) return <div className="main-view" />;
 
     return (
       <div className="main-view">
-        {movies.map(movie => (
-          <div className="movie-card" key={movie._id}>{movie.Title}</div>
-        ))}
+        {selectedMovie
+          ? <MovieView movie={selectedMovie} />
+          : movies.map(movie => (
+            <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)} />
+          ))
+        }
       </div>
     );
   }
