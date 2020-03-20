b0f4bc4aa8e4d697b865eb03d51caf6a92b100e3
joelcmk <57118300+joelcmk@users.noreply.github.com>
2020-03-20 17:14:32 -0500

    added jwt auth

--- index.js
+++ index.js
@@ -43,6 +43,26 @@ app.use(
 );
 */
 
+// Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:1234');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 // Gets the list of data about all the movies
 app.get("/movies", passport.authenticate('jwt', { session: false }), function (req, res) {
   Movies.find()
